ce009d218539ca4f04729150e7b8c195c945909f
GaspardRuan <1039553124@qq.com>
2025-06-26 02:01:20 +0800

    Initial Commit

---
+++ NuitComic/NuitComicApp.swift
@@ -0,0 +1,17 @@
+//
+//  NuitComicApp.swift
+//  NuitComic
+//
+//  Created by Zhongqiu Ruan on 2025/6/26.
+//
+
+import SwiftUI
+
+@main
+struct NuitComicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
