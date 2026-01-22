73b24fe95ed75f06e91d78a3966353c073f23d10
GaspardRuan <1039553124@qq.com>
2026-01-22 18:13:45 +0800

    Initial Commit

---
+++ nuitcomic/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  nuitcomic
+//
+//  Created by Zhongqiu Ruan on 2026/1/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ nuitcomic/nuitcomicApp.swift
@@ -0,0 +1,17 @@
+//
+//  nuitcomicApp.swift
+//  nuitcomic
+//
+//  Created by Zhongqiu Ruan on 2026/1/22.
+//
+
+import SwiftUI
+
+@main
+struct nuitcomicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
